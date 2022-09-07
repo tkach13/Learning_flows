3dd2e3768f8efb50499a4ccfe6d49fd7ed40f762
Tornike kachkachishvili <tkachkachishvili@tbcbank.com.gegit config --global user.email tkachkachishvili@tbcbank.com.gegit config --global user.email tkachkachishvili@tbcbank.com.ge>
2022-09-07 18:13:32 +0400

    Initial commit

---
+++ app/src/main/java/com/benten/learningkotlin/LearningKotlin.kt
@@ -0,0 +1,27 @@
+package com.benten.learningkotlin
+
+import kotlinx.coroutines.*
+import kotlinx.coroutines.flow.asFlow
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.flow.filter
+
+
+fun main() = runBlocking {
+    forceUsers.asFlow()
+        .collect {
+            log(it.name)
+        }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
