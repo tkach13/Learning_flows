2b4acef6b0c6f746a6ed051919e4766524080a0e
Tornike kachkachishvili <tkachkachishvili@tbcbank.com.gegit config --global user.email tkachkachishvili@tbcbank.com.gegit config --global user.email tkachkachishvili@tbcbank.com.ge>
2022-09-07 18:15:57 +0400

    deleted

--- app/src/main/java/com/benten/learningkotlin/LearningKotlin.kt
+++ app/src/main/java/com/benten/learningkotlin/LearningKotlin.kt
@@ -1,9 +1,7 @@
 package com.benten.learningkotlin
 
 import kotlinx.coroutines.*
-import kotlinx.coroutines.flow.asFlow
-import kotlinx.coroutines.flow.collect
-import kotlinx.coroutines.flow.filter
+
 
 
 fun main() = runBlocking {
